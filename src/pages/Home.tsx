b8748d584c266d68d86a32736099be690b75aaad
Alex Santos de Souza <41313437+Alexssmusica@users.noreply.github.com>
2021-06-22 23:50:55 -0300

    Aula 03 – In Orbit Part-1

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,7 @@
+import { FormEvent, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 
+import { database } from '../services/firebase';
 import { useAuth } from '../hooks/useAuth';
 import { Button } from '../components/Button';
 import illustrationImg from '../assets/images/illustration.svg';
@@ -10,6 +12,7 @@ import '../styles/auth.scss';
 export function Home() {
     const history = useHistory();
     const { user, signInWithGoogle } = useAuth();
+    const [roomCode, setRoomCode] = useState('');
 
     async function handleCreateRoom() {
         if (!user) {
@@ -17,6 +20,23 @@ export function Home() {
         }
 
         history.push('/rooms/new');
+    }
+
+    async function handleJoinRoom(event: FormEvent) {
+        event.preventDefault();
+
+        if (roomCode.trim() === '') {
+            return;
+        }
+
+        const roomRef = await database.ref(`rooms/${roomCode}`).get();
+
+        if (!roomRef.exists()) {
+            alert('Room does not exist.');
+            return;
+        }
+
+        history.push(`/rooms/${roomCode}`);
     }
 
     return (
@@ -34,8 +54,13 @@ export function Home() {
                         Crie sua sala com o Google
                     </button>
                     <div className="separator">ou entre em uma sala</div>
-                    <form>
-                        <input type="text" placeholder="Digite o código da sala" />
+                    <form onSubmit={handleJoinRoom}>
+                        <input
+                            type="text"
+                            placeholder="Digite o código da sala"
+                            onChange={(event) => setRoomCode(event.target.value)}
+                            value={roomCode}
+                        />
                         <Button type="submit">Entrar na sala</Button>
                     </form>
                 </div>
