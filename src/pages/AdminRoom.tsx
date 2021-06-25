dc8bdfbd63966e93e5fa040def6248aa43cd97b9
Alex Santos de Souza <41313437+Alexssmusica@users.noreply.github.com>
2021-06-24 23:43:26 -0300

    Aula 05 - Surface Exploration

--- src/pages/AdminRoom.tsx
+++ src/pages/AdminRoom.tsx
@@ -1,12 +1,13 @@
 import { useHistory, useParams } from 'react-router-dom';
+import { database } from '../services/firebase';
 import { useRoom } from '../hooks/useRoom';
 import { Question } from '../components/Question';
 import { Button } from '../components/Button';
 import { RoomCode } from '../components/RoomCode';
 import logoImg from '../assets/images/logo.svg';
 import deleteImg from '../assets/images/delete.svg';
+import checkImg from '../assets/images/check.svg';
 import '../styles/room.scss';
-import { database } from '../services/firebase';
 
 type RoomParams = {
     id: string;
@@ -27,6 +28,18 @@ export function AdminRoom() {
         });
 
         history.push('/');
+    }
+
+    async function handleCheckQuestionAsAnswered(questionId: string) {
+        await database.ref(`rooms/${roomId}/questions/${questionId}`).update({
+            isAnswered: true
+        });
+    }
+
+    async function handleHighlightQuestion(questionId: string) {
+        await database.ref(`rooms/${roomId}/questions/${questionId}`).update({
+            isHighlighted: true
+        });
     }
 
     async function handleDeleteQuestion(questionId: string) {
@@ -58,7 +71,47 @@ export function AdminRoom() {
                 <div className="question-list">
                     {questions.map((question) => {
                         return (
-                            <Question content={question.content} author={question.author} key={question.id}>
+                            <Question
+                                content={question.content}
+                                author={question.author}
+                                isHighlighted={question.isHighlighted}
+                                isAnswered={question.isAnswered}
+                                key={question.id}
+                            >
+                                {!question.isAnswered && (
+                                    <>
+                                        <button
+                                            type="button"
+                                            onClick={() => handleCheckQuestionAsAnswered(question.id)}
+                                        >
+                                            <img src={checkImg} alt="Marcar pergunta como respondida" />
+                                        </button>
+                                        <button
+                                            type="button"
+                                            onClick={() => handleHighlightQuestion(question.id)}
+                                        >
+                                            <div className="highlight-img">
+                                                <svg
+                                                    width="24"
+                                                    height="24"
+                                                    viewBox="0 0 24 24"
+                                                    fill="none"
+                                                    xmlns="http://www.w3.org/2000/svg"
+                                                >
+                                                    <path
+                                                        fillRule="evenodd"
+                                                        clipRule="evenodd"
+                                                        d="M12 17.9999H18C19.657 17.9999 21 16.6569 21 14.9999V6.99988C21 5.34288 19.657 3.99988 18 3.99988H6C4.343 3.99988 3 5.34288 3 6.99988V14.9999C3 16.6569 4.343 17.9999 6 17.9999H7.5V20.9999L12 17.9999Z"
+                                                        stroke="#737380"
+                                                        strokeWidth="1.5"
+                                                        strokeLinecap="round"
+                                                        strokeLinejoin="round"
+                                                    />
+                                                </svg>
+                                            </div>
+                                        </button>
+                                    </>
+                                )}
                                 <button type="button" onClick={() => handleDeleteQuestion(question.id)}>
                                     <img src={deleteImg} alt="Remover pergunta" />
                                 </button>
